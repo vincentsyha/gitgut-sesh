9953e4f52815ed12438019153f8110f92201335e
Vincent Ha <vincentsyha@gmail.com>
2022-05-18 11:51:48 -0600

    feat(add): updated function to include docstring

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 def multiply(num1, num2, num3):
     return num1 * num2 * num3
 
 def add(num1,num2):
+    '''Function which takes in 2 numbers and returns their sum'''
     return num1 + num2
->>>>>>> 6f6fee8 (feat: adds addition function)
